0d5c15b31808ecbf89cf61d8d97547f74499d8e9
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-08-26 12:34:02 +0200

    Fixes

--- core/jvm/test/fuzz/DiffTest.kt
+++ core/jvm/test/fuzz/DiffTest.kt
@@ -9,107 +9,36 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider
 import com.code_intelligence.jazzer.junit.FuzzTest
 import fuzz.utils.compareTest
 import kotlinx.datetime.*
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Timeout
+import org.junit.jupiter.api.BeforeAll
 import java.time.Period
 import java.time.temporal.ChronoUnit
-import java.util.concurrent.TimeUnit
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.Path
+import kotlin.io.path.deleteRecursively
 import kotlin.test.assertEquals
 import kotlin.time.toKotlinDuration
 
 class DiffTest {
-    class datePeriodDiff {
-        @Timeout(value = 1, unit = TimeUnit.SECONDS)
-        @FuzzTest(/*maxDuration = "2h"*/)
-        fun diff(data: FuzzedDataProvider) {
-            val mod = 0 //5000
-            val a = data.consumeDate(-mod, mod)
-            val b = data.consumeDate(-mod, mod)
-
-            val kotlinRes = runCatching<DatePeriod> { a.periodUntil(b) }
-            val javaRes = runCatching<Period> { a.copyj().until(b.copyj()) }
-
-            assertEquals(kotlinRes.isSuccess, javaRes.isSuccess)
-
-            if (kotlinRes.isFailure || javaRes.isFailure) return
-
-            val kotlinVal = kotlinRes.getOrThrow()
-            val javaVal = javaRes.getOrThrow()
-
-            val javaFromKotlin = kotlinVal.toJavaPeriod()
-            val kotlinFromJava = javaVal.toKotlinDatePeriod()
-
-            assertEquals(kotlinVal, kotlinFromJava)
-            assertEquals(javaVal, javaFromKotlin)
+    companion object {
+        @OptIn(ExperimentalPathApi::class)
+        @JvmStatic
+        @BeforeAll
+        fun cleanCorpus() {
+            Path(".cifuzz-corpus").deleteRecursively()
         }
     }
 
-    class datePeriodDiffNonNegative {
-        @FuzzTest(maxDuration = "2h")
-        fun diff(data: FuzzedDataProvider) {
-            val aa = data.consumeDate()
-            val bb = data.consumeDate()
-
-            val a = if (aa < bb) aa else bb
-            val b = if (aa < bb) bb else aa
-
-            val kotlinRes = runCatching<DatePeriod> { a.periodUntil(b) }
-            val javaRes = runCatching<Period> { a.copyj().until(b.copyj()) }
-
-            assertEquals(kotlinRes.isSuccess, javaRes.isSuccess)
-
-            if (kotlinRes.isFailure || javaRes.isFailure) return
-
-            val kotlinVal = kotlinRes.getOrThrow()
-            val javaVal = javaRes.getOrThrow()
-
-            val javaFromKotlin = kotlinVal.toJavaPeriod()
-            val kotlinFromJava = javaVal.toKotlinDatePeriod()
-
-            assertEquals(kotlinVal, kotlinFromJava)
-            assertEquals(javaVal, javaFromKotlin)
-        }
-    }
-
-    class datePeriodDiffNonPositive {
-        @FuzzTest(maxDuration = "2h")
-        fun diff(data: FuzzedDataProvider) {
-            val aa = data.consumeDate()
-            val bb = data.consumeDate()
-
-            val a = if (aa > bb) aa else bb
-            val b = if (aa > bb) bb else aa
-
-            val kotlinRes = runCatching<DatePeriod> { a.periodUntil(b) }
-            val javaRes = runCatching<Period> { a.copyj().until(b.copyj()) }
-
-            assertEquals(kotlinRes.isSuccess, javaRes.isSuccess)
-
-            if (kotlinRes.isFailure || javaRes.isFailure) return
-
-            val kotlinVal = kotlinRes.getOrThrow()
-            val javaVal = javaRes.getOrThrow()
-
-            val javaFromKotlin = kotlinVal.toJavaPeriod()
-            val kotlinFromJava = javaVal.toKotlinDatePeriod()
-
-            assertEquals(kotlinVal, kotlinFromJava)
-            assertEquals(javaVal, javaFromKotlin)
-        }
-    }
-
-    @Test
-    fun reproduce() {
-        val first = "0000-05-31"
-        val second = "0000-04-01"
-
-        val a = LocalDate.parse(first)
-        val b = LocalDate.parse(second)
+    @FuzzTest(/*maxDuration = "2h"*/)
+    fun diffDatePeriod(data: FuzzedDataProvider) {
+        val mod = 0 //5000
+        val a = data.consumeDate(-mod, mod)
+        val b = data.consumeDate(-mod, mod)
 
         val kotlinRes = runCatching<DatePeriod> { a.periodUntil(b) }
         val javaRes = runCatching<Period> { a.copyj().until(b.copyj()) }
 
         assertEquals(kotlinRes.isSuccess, javaRes.isSuccess)
+
         if (kotlinRes.isFailure || javaRes.isFailure) return
 
         val kotlinVal = kotlinRes.getOrThrow()
@@ -122,76 +51,93 @@ class DiffTest {
         assertEquals(javaVal, javaFromKotlin)
     }
 
-    class instantDiff {
-        @FuzzTest(maxDuration = "2h")
-        fun diff(data: FuzzedDataProvider) = with(data) {
-            val kfirst = consumeInstant()
-            val ksecond = consumeInstant()
+    @FuzzTest(maxDuration = "2h")
+    fun diffDatePeriodNonNegative(data: FuzzedDataProvider) {
+        val aa = data.consumeDate()
+        val bb = data.consumeDate()
 
-            val jfirst = kfirst.copyj()
-            val jsecond = ksecond.copyj()
+        val a = if (aa < bb) aa else bb
+        val b = if (aa < bb) bb else aa
 
-            compareTest(
-                createKotlin = { kfirst.until(ksecond, DateTimeUnit.SECOND) },
-                createJava = {
-                    jfirst.until(jsecond, ChronoUnit.SECONDS)
-                },
-                kotlinToJava = { it },
-                javaToKotlin = { it }
-            )
-        }
+        val kotlinRes = runCatching<DatePeriod> { a.periodUntil(b) }
+        val javaRes = runCatching<Period> { a.copyj().until(b.copyj()) }
+
+        assertEquals(kotlinRes.isSuccess, javaRes.isSuccess)
+
+        if (kotlinRes.isFailure || javaRes.isFailure) return
+
+        val kotlinVal = kotlinRes.getOrThrow()
+        val javaVal = javaRes.getOrThrow()
+
+        val javaFromKotlin = kotlinVal.toJavaPeriod()
+        val kotlinFromJava = javaVal.toKotlinDatePeriod()
+
+        assertEquals(kotlinVal, kotlinFromJava)
+        assertEquals(javaVal, javaFromKotlin)
     }
 
-    class instantTZDiff {
-        @FuzzTest(maxDuration = "2h")
-        fun diff(data: FuzzedDataProvider) = with(data) {
-            val kfirst = consumeInstant()
-            val ksecond = consumeInstant()
 
-            val ktz = consumeTimeZone()
-            val jtz = ktz.toJavaZoneId()
+    @FuzzTest(maxDuration = "1m")
+    fun diffInstant(data: FuzzedDataProvider) = with(data) {
+        val kfirst = consumeInstant()
+        val ksecond = consumeInstant()
 
-            val jfirst = kfirst.copyj().atZone(jtz)
-            val jsecond = ksecond.copyj().atZone(jtz)
+        val jfirst = kfirst.copyj()
+        val jsecond = ksecond.copyj()
 
-
-            compareTest(
-                createKotlin = { kfirst.until(ksecond, DateTimeUnit.SECOND, ktz) },
-                createJava = {
-                    jfirst.until(jsecond, ChronoUnit.SECONDS)
-                },
-                kotlinToJava = { it },
-                javaToKotlin = { it }
-            )
-        }
+        compareTest(
+            createKotlin = { kfirst.until(ksecond, DateTimeUnit.SECOND) },
+            createJava = { jfirst.until(jsecond, ChronoUnit.SECONDS) },
+            kotlinToJava = { it },
+            javaToKotlin = { it }
+        )
     }
 
-    class InstantToDateTimeAndBack {
-        @FuzzTest(maxDuration = "2h")
-        fun test(data: FuzzedDataProvider) = with(data) {
-            val d = consumeDateTime()
-            val tz = consumeTimeZone()
-            compareTest(
-                createKotlin = { d },
-                createJava = { d.toInstant(tz).toLocalDateTime(tz) },
-                javaToKotlin = { it },
-                kotlinToJava = { it }
-            )
-        }
+    @FuzzTest(maxDuration = "2h")
+    fun diffInstantWithZoneId(data: FuzzedDataProvider) = with(data) {
+        val kfirst = consumeInstant()
+        val ksecond = consumeInstant()
+
+        val ktz = consumeTimeZone()
+        val jtz = ktz.toJavaZoneId()
+
+        val jfirst = kfirst.copyj().atZone(jtz)
+        val jsecond = ksecond.copyj().atZone(jtz)
+
+
+        compareTest(
+            createKotlin = { kfirst.until(ksecond, DateTimeUnit.SECOND, ktz) },
+            createJava = {
+                jfirst.until(jsecond, ChronoUnit.SECONDS)
+            },
+            kotlinToJava = { it },
+            javaToKotlin = { it }
+        )
     }
 
-    class CompareDurations {
-        @FuzzTest(maxDuration = "2h")
-        fun test(data: FuzzedDataProvider) = with(data) {
-            val first = consumeInstant(nanoFrom = 0, nanoTo = 0)
-            val second = consumeInstant(nanoFrom = 0, nanoTo = 0)
-
-            compareTest(
-                createKotlin = { second - first },
-                createJava = { java.time.Duration.between(first.copyj(), second.copyj()) },
-                kotlinToJava = { it.copyj() },
-                javaToKotlin = { it.toKotlinDuration() }
-            )
-        }
+    @FuzzTest(maxDuration = "2h")
+    fun diffDateTimeTimeZone(data: FuzzedDataProvider) = with(data) {
+        val d = consumeDateTime()
+        val tz = consumeTimeZone()
+        compareTest(
+            createKotlin = { d },
+            createJava = { d.toInstant(tz).toLocalDateTime(tz) },
+            javaToKotlin = { it },
+            kotlinToJava = { it }
+        )
     }
+
+    @FuzzTest(maxDuration = "2h")
+    fun diffInstantNoNanos(data: FuzzedDataProvider) = with(data) {
+        val first = consumeInstant(nanoFrom = 0, nanoTo = 0)
+        val second = consumeInstant(nanoFrom = 0, nanoTo = 0)
+
+        compareTest(
+            createKotlin = { second - first },
+            createJava = { java.time.Duration.between(first.copyj(), second.copyj()) },
+            kotlinToJava = { it.copyj() },
+            javaToKotlin = { it.toKotlinDuration() }
+        )
+    }
+
 }
